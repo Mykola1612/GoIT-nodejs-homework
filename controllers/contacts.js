1f52ba639bede7d668764c08188714b0879f8db0
Mykola1612 <fermer2007pavlovich@gmail.com>
2024-01-22 22:18:14 +0100

    Update contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,7 +3,7 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const listContacts = async (req, res, next) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 20 } = req.query;
   const skip = (page - 1) * limit;
   const result = await contactsDB.find({ owner }, "", { skip, limit });
   res.status(200).json(result);
